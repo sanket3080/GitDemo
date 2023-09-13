43d081b966ebb9131bb97d43f06ac8d7e07cbf05
Sanket Sakore <sanket.sakore@gmail.com>
2023-09-13 16:18:21 +0530

    this is commit from IST Time

--- src/com/java/Student.java
+++ src/com/java/Student.java
@@ -8,6 +8,7 @@ public class Student {
 		Student s1=new Student();
 		System.out.println("Student Id: "+s1.id);
 		System.out.println("Student Name: "+s1.name);
+		System.out.println("ID: "+s1.id+" Name: "+s1.name);
 		
 		
 	}
